0a388ead9b4727bd56f3a02bea682db359a3d9ba
sunxihao <sunxh2016@lzu.edu.cn>
2025-01-16 22:30:01 +0800

    custom_step failed

--- src/llamafactory/train/sft/trainer.py
+++ src/llamafactory/train/sft/trainer.py
@@ -73,72 +73,7 @@ class CustomSeq2SeqTrainer(Seq2SeqTrainer):
     def create_optimizer(self) -> "torch.optim.Optimizer":
         if self.optimizer is None:
             self.optimizer = create_custom_optimizer(self.model, self.args, self.finetuning_args)
-        self.optimizer = super().create_optimizer()
-
-        def custom_step(self, closure: Callable = None):
-            """
-            Performs a single optimization step.
-
-            Arguments:
-                closure (`Callable`, *optional*): A closure that reevaluates the model and returns the loss.
-            """
-            loss = None
-            if closure is not None:
-                loss = closure()
-
-            for group in self.param_groups:
-                for p in group["params"]:
-                    print("sean: ", p.name)
-                    if p.grad is None:
-                        continue
-                    grad = p.grad
-                    if grad.is_sparse:
-                        raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
-
-                    state = self.state[p]
-
-                    # State initialization
-                    if len(state) == 0:
-                        state["step"] = 0
-                        # Exponential moving average of gradient values
-                        state["exp_avg"] = torch.zeros_like(p)
-                        # Exponential moving average of squared gradient values
-                        state["exp_avg_sq"] = torch.zeros_like(p)
-
-                    exp_avg, exp_avg_sq = state["exp_avg"], state["exp_avg_sq"]
-                    beta1, beta2 = group["betas"]
-
-                    state["step"] += 1
-
-                    # Decay the first and second moment running average coefficient
-                    # In-place operations to update the averages at the same time
-                    exp_avg.mul_(beta1).add_(grad, alpha=(1.0 - beta1))
-                    exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1.0 - beta2)
-                    denom = exp_avg_sq.sqrt().add_(group["eps"])
-
-                    step_size = group["lr"]
-                    if group["correct_bias"]:  # No bias correction for Bert
-                        bias_correction1 = 1.0 - beta1 ** state["step"]
-                        bias_correction2 = 1.0 - beta2 ** state["step"]
-                        step_size = step_size * math.sqrt(bias_correction2) / bias_correction1
-
-                    p.addcdiv_(exp_avg, denom, value=-step_size)
-
-                    # Just adding the square of the weights to the loss function is *not*
-                    # the correct way of using L2 regularization/weight decay with Adam,
-                    # since that will interact with the m and v parameters in strange ways.
-                    #
-                    # Instead we want to decay the weights in a manner that doesn't interact
-                    # with the m/v parameters. This is equivalent to adding the square
-                    # of the weights to the loss with plain (non-momentum) SGD.
-                    # Add weight decay at the end (fixed version)
-                    if group["weight_decay"] > 0.0:
-                        p.add_(p, alpha=(-group["lr"] * group["weight_decay"]))
-
-            return loss
-
-        self.optimizer.step = custom_step
-        return self.optimizer
+        return super().create_optimizer()
 
     @override
     def create_scheduler(
